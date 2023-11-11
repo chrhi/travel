99cd89e6efb4f7d5214dd5a1fdf10659a7810c95
abdellah <Abdellah.chehri14@gmail.com>
2023-11-11 17:03:59 +0100

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
-import './globals.css'
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
-}
+  title: "Travel",
+  description: "Travel App for Camping",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden">
-          {children}
-        </main>
+        <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
